36de8841c7bb7a3956a06c3aeb51efa603a5c353
Dhruv Karandikar <dhruvkarandikar@KTSPL-M-107.local>
2024-02-04 16:33:29 +0530

    change

--- auto_email.py
+++ auto_email.py
@@ -1,14 +1,25 @@
 import smtplib
 
+''' This Email generator is only for using email that are used to give data as an automator.
+    Don't use personal email addresses for sending emails library not secure.
+    Use 2 -factor authentication in email addresses instead to use your own personal email or make new email id.
+'''
 
-def send_email(sender_email, secret_password, receiver_email, message):
+send_email_to = input('Send email to: ')
+sender_email = input("Your email id: ")
+password = input("Your password: ")
+your_message = input('Your message: ')
+
+
+
+def send_email(your_email, secret_password, to_email, message):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
 
-    server.login(user=sender_email, password=secret_password)
-    server.sendmail(sender_email, to_addrs=receiver_email, msg=message)
+    server.login(user=your_email, password=secret_password)
+    server.sendmail(your_email, to_addrs=to_email, msg=message)
 
 
-send_email()
+send_email(your_email=sender_email, secret_password=password, to_email=send_email_to, message=your_message)
 
 
